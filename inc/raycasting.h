c3773db1b6c38e8e0e5d18e9f328e5a78de2283c
RubBarkhudaryan <rubenbarkhudaryan2005@gmail.com>
2026-01-24 18:10:48 +0400

    some ethical changes

--- inc/raycasting.h
+++ inc/raycasting.h
@@ -6,16 +6,16 @@
 /*   By: rbarkhud <rbarkhud@student.42yerevan.am    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 17:40:59 by apatvaka          #+#    #+#             */
-/*   Updated: 2026/01/23 21:58:33 by rbarkhud         ###   ########.fr       */
+/*   Updated: 2026/01/24 18:05:41 by rbarkhud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RAYCASTING_H
 # define RAYCASTING_H
 
-# include "../inc/cub.h"
-# include "../inc/init.h"
-# include "../inc/struct.h"
+# include "./cub.h"
+# include "./init.h"
+# include "./struct.h"
 
 void	start_game_loop(t_game *game);
 int		render(void *ptr);
